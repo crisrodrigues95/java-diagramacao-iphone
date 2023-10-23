e4d65e71db95e7cf506db9647524d5a23e014e08
Cristhian Rodrigues <cristhian.rodrigues95@gmail.com>
2023-10-22 21:38:08 -0300

    update

--- src/model/Browser.java
+++ src/model/Browser.java
@@ -1,5 +1,10 @@
 package model;
 
 public interface Browser {
+	
+	public void exibirPagina (String Url) ;
+	public void adicionarNovaAba(String Url);
+	public void atualizarPagina ();
+
 
 }
--- src/model/Iphone.java
+++ src/model/Iphone.java
@@ -1,5 +1,59 @@
 package model;
 
-public class Iphone {
+public class Iphone implements Browser, MusicPlayer, Telephone{
+
+	@Override
+	public void ligar(String numero) {
+		System.out.println("Ligando para:" + numero );
+		
+	}
+
+	@Override
+	public void atender(String numero) {
+		System.out.println("Falando com:" + numero );
+		
+	}
+
+	@Override
+	public void iniciarCorreioVoz(String numero) {
+		System.out.println("Mensagem de voz de:" + numero );
+		
+	}
+
+	@Override
+	public void tocar(String musica) {
+		System.out.println("Tocando:" + musica );
+		
+	}
+
+	@Override
+	public void pausar(String musica) {
+		System.out.println("Pausando:" + musica );
+		
+	}
+
+	@Override
+	public void selecionarMusica(String musica) {
+		System.out.println("Escolhida:" + musica );
+		
+	}
+
+	@Override
+	public void exibirPagina(String url) {
+		System.out.println("Mostrando:" + url );
+		
+	}
+
+	@Override
+	public void adicionarNovaAba(String url) {
+		System.out.println("Movendo para" + url );
+		
+	}
+
+	@Override
+	public void atualizarPagina() {
+		
+		
+	}
 
 }
